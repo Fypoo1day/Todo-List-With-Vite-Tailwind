b3fe8e2b219d53caca0986f66a4f6935b57b569d
Fypoolday <Fypoolday@users.noreply.github.com>
2022-07-08 13:28:58 -0300

    Programo el Listado de las tareas

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 
 const taskForm = document.querySelector<HTMLFormElement>("#taskForm");
+const taskList = document.querySelector<HTMLDivElement>("#tasksList");
 
 interface Task {
   title: string;
@@ -21,4 +22,46 @@ taskForm?.addEventListener("submit", (e) => {
   });
 
   localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  renderTasks(tasks);
+
+  taskForm.reset();
+  title.focus();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  renderTasks(tasks);
+});
+
+function renderTasks(tasks: Task[]) {
+  taskList!.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const taskElement = document.createElement("div");
+    taskElement.className =
+      "bg-zinc-800 mb-1 p-4 rounded-lg hover:bg-zinc-700 hover:cursor-pointer";
+
+    const header = document.createElement("header");
+    header.className = "flex justify-between";
+
+    const title = document.createElement("span");
+    title.innerText = task.title;
+
+    const btnDelete = document.createElement("button");
+    btnDelete.className = "bg-red-500 px-2 py-1 rounded-md";
+    btnDelete.innerText = "Delete";
+
+    header.append(title);
+
+    header.append(btnDelete);
+
+    const description = document.createElement("p");
+    description.innerText = task.description;
+
+    taskElement.append(header);
+    taskElement.append(description);
+
+    taskList?.append(taskElement);
+  });
+}
